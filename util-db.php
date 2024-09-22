93a010c64508f49e10ed803046a38829e27b4163
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-22 17:21:46 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli(' 129.15.65.230', 'mis4013db', 'Hippolove123#', '4013Azure');
+    $conn = new mysqli(' 129.15.65.230', 'mis4013db', 'Hippolove123#', 'mycoffeeshop_database');
     
     // Check connection
     if ($conn->connect_error) {
