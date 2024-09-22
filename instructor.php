678c034b7aabdcef272a6185e837bab804386ffe
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-22 17:06:25 -0500

    Update and rename post.php to instructor.php

---
+++ instructor.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once ('util-db.php');
+require_once('model-instructors');
+
+$pageTitle = "Instructors";
+include 'view-header.php';
+$instructors = selectInstructors();
+include 'view-instructors.php;
+include 'view-footer.php';
+?>
