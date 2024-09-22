78cb1be426a24e12752b7c80de54889aa5b47505
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-22 17:31:53 -0500

    Create view-instructors.php

---
+++ view-instructors.php
@@ -0,0 +1,26 @@
+<h1>Instructors  </h1>
+<div class="table-responsive">
+  <table class="table">
+   <thead>
+     <tr>
+       <th> ID </th>
+     <th>Name</th>
+     <th>office</th>
+     </tr>
+     
+   </thead> 
+    <tbody>
+      <?php
+while ($instructor = $instructors -> fetch_assoc() {
+    ?>
+       <tr>
+         <td><?php echo $instructor['customer_id'];?> </td>
+         <td><?php echo $instructor['firstname'];?>  </td>
+         <td><?php echo $instructor['address'];?>  </td>
+       </tr>
+       <?php
+}
+      ?>  
+    </tbody>
+  </table>
+</div>
