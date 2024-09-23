ba2b677193afb6d0a41b96e80ada749d9fba4674
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-23 13:02:35 -0500

    Create model-courses-by-instructor.php

---
+++ model-courses-by-instructor.php
@@ -0,0 +1,18 @@
+
+<?php
+
+function selectCoursesByInstructor($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("select o.order_id, customer_id, order_date, total_amount, status from orders o join order_details od on o.order_id = od.order_id where o.customer_id=? ");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
