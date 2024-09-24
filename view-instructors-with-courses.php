1060d739b81f6e7ae7eaef3edaef225572d48181
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-24 12:20:33 -0500

    Update view-instructors-with-courses.php

--- view-instructors-with-courses.php
+++ view-instructors-with-courses.php
@@ -9,13 +9,18 @@ while ($instructor = $instructors -> fetch_assoc() {
     <div class="card-body">
       <h5 class="card-title"><?php echo $instructor['firstname'];?> e</h5>
       <p class="card-text">
+         <ul class="list-group">
+      
         <?php
        $courses=selectCoursesByInstructor($instructor['customer_id']);
         while($course= $courses -> fetch_assoc()){
           ?>
+          <li class="list-group-item"><?php echo $course['order_id'];?> -<?php echo $course['status'];?> -<?php echo $course['quantity'];?> - <?php echo $course['price'];?>   </li>
+
           <?php
         }
         ?>
+         </ul> 
       </p>
       <p class="card-text"><small class="text-body-secondary">Office: <?php echo $instructor['address'];?></small></p>
     </div>
