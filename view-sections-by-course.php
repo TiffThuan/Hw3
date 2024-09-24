359a16d99faa4d57189f71d9385a5ea602f0d331
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-24 00:10:06 -0500

    Create view-sections-by-course.php

---
+++ view-sections-by-course.php
@@ -0,0 +1,31 @@
+<h1>Sections By Course  </h1>
+<div class="table-responsive">
+  <table class="table">
+   <thead>
+     <tr>
+       <th> ID </th>
+     <th>Total Amount</th>
+     <th>Status</th>
+     <th>Quantity</th>
+     <th>Price</th>
+     </tr>
+     
+   </thead> 
+    <tbody>
+      <?php
+while ($section= $sections -> fetch_assoc() {
+    ?>
+       <tr>
+         <td><?php echo $section['order_id'];?> </td>
+         <td><?php echo $section['total_amount'];?>  </td>
+         <td><?php echo $section['status'];?>  </td>
+         <td><?php echo $section['quantity'];?>  </td>
+         <td><?php echo $section['price'];?>  </td>
+        
+       </tr>
+       <?php
+}
+      ?>  
+    </tbody>
+  </table>
+</div>
