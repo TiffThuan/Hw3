38b3bdf1e1d91e6a3cdc862ba43588336630130c
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-24 12:13:24 -0500

    Create view-instructors-with-courses.php

---
+++ view-instructors-with-courses.php
@@ -0,0 +1,27 @@
+<h1>Instructors with Courses </h1>
+<div class="card-group">
+<?php
+
+while ($instructor = $instructors -> fetch_assoc() {
+?>
+         <div class="card">
+   
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $instructor['firstname'];?> e</h5>
+      <p class="card-text">
+        <?php
+       $courses=selectCoursesByInstructor($instructor['customer_id']);
+        while($course= $courses -> fetch_assoc()){
+          ?>
+          <?php
+        }
+        ?>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Office: <?php echo $instructor['address'];?></small></p>
+    </div>
+  </div>
+
+<?php
+}
+?>  
+</div>
