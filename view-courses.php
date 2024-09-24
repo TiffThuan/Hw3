cd4f225fd3d5ea19b3758179b88b1b328c235e0d
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-24 00:20:52 -0500

    Update view-courses.php

--- view-courses.php
+++ view-courses.php
@@ -6,19 +6,26 @@
        <th> ID </th>
      <th>Total Amount</th>
      <th>Status</th>
+       <th></th>
        
      </tr>
      
    </thead> 
     <tbody>
       <?php
-while ($instructor = $instructors -> fetch_assoc() {
+while ($course = $courses -> fetch_assoc() {
     ?>
        <tr>
          <td><?php echo $course['order_id'];?> </td>
          <td><?php echo $course['total_amount'];?>  </td>
          <td><?php echo $course['status'];?>  </td>
-    
+         <td>
+           <form method ="post" action ="sections-by-course.php">
+             <input type ="hidden" name ="cid" value = "<?php echo $course['order_id'];?> ">
+            <div class="mb-3">
+            <button type="submit" class="btn btn-primary">Section</button>
+          </form>
+         </td>
        </tr>
        <?php
 }
