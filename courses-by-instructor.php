a6578aace57cd15c68de71fa69e06ecb24510899
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-25 13:21:44 -0500

    Update courses-by-instructor.php

--- courses-by-instructor.php
+++ courses-by-instructor.php
@@ -1,11 +1,13 @@
-<?php
 
+  <?php
 require_once ('util-db.php');
 require_once('model-courses-by-instructor.php');
 
 $pageTitle = "Course by Instructor";
 include 'view-header.php';
-$instructors = selectCoursesByInstructor($_Get['id']);
-include 'view courses-by-instructor.php;
+
+// Correcting $_Get to $_GET
+$instructors = selectCoursesByInstructor($_GET['id']);
+include 'view-courses-by-instructor.php'; // Closing quote added
 include 'view-footer.php';
 ?>
