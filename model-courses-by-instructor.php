0d45b71eb0ac8a25450122ca2edfdf0c399073c6
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-25 13:30:15 -0500

    Update model-courses-by-instructor.php

--- model-courses-by-instructor.php
+++ model-courses-by-instructor.php
@@ -1,10 +1,13 @@
 
-<?php
 
+<?php
 function selectCoursesByInstructor($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select o.order_id, customer_id, order_date, total_amount, status from orders o join order_details od on o.order_id = od.order_id where o.customer_id=? ");
+        $stmt = $conn->prepare("SELECT o.order_id, customer_id, order_date, total_amount, status 
+                                FROM orders o 
+                                JOIN order_details od ON o.order_id = od.order_id 
+                                WHERE o.customer_id = ?");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
