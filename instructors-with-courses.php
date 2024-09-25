df9af3e1f4eb589d23d0a82c4d34e89753ff2ec0
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-25 13:28:36 -0500

    Update instructors-with-courses.php

--- instructors-with-courses.php
+++ instructors-with-courses.php
@@ -1,11 +1,12 @@
-<?php
 
+
+  <?php
 require_once ('util-db.php');
-require_once('model-instructors-with-courses');
+require_once('model-instructors-with-courses.php'); // Corrected filename
 
 $pageTitle = "Instructors With Courses";
 include 'view-header.php';
 $instructors = selectInstructors();
-include 'view-instructors-with-course.php;
+include 'view-instructors-with-courses.php'; // Corrected file path
 include 'view-footer.php';
 ?>
