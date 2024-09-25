aee9a332e2b7cabd08efc6f1e7db147f1c7033e4
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-25 13:31:57 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,11 +1,11 @@
 
-<?php
 
+<?php
 function selectCourses() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select order_id, customer_id, order_date, total_amount, status from 'orders' ");
-    
+        $stmt = $conn->prepare("SELECT order_id, customer_id, order_date, total_amount, status 
+                                FROM orders");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -16,3 +16,4 @@ function selectCourses() {
     }
 }
 ?>
+
