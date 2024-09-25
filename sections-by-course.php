5f1169a1eb114829867009631a24daacbb22f3e1
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-25 13:33:35 -0500

    Update sections-by-course.php

--- sections-by-course.php
+++ sections-by-course.php
@@ -1,11 +1,12 @@
-<?php
 
+
+  <?php
 require_once ('util-db.php');
 require_once('model-sections-by-course.php');
 
 $pageTitle = "Section by Course";
 include 'view-header.php';
-$sections = selectSectionsByCourse($_Post['cid']);
-include 'view sections-by-course.php;
+$sections = selectSectionsByCourse($_POST['cid']); // Correct $_Post to $_POST
+include 'view-sections-by-course.php'; // Closing quote added
 include 'view-footer.php';
 ?>
