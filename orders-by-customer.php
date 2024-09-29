24517e47098c04af8201195b24238dd6a54732c8
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 17:38:37 -0500

    Update orders-by-customer.php

--- orders-by-customer.php
+++ orders-by-customer.php
@@ -6,7 +6,8 @@ require_once('model-orders-by-customer.php');
 $pageTitle = "Orders by Customer"; // Update title
 include 'view-header.php';
 
-$orders = selectOrdersByCustomer($_GET['id']); // Use customer_id instead of instructor id
+$customer_id = $_GET['id']; // Get the customer_id from the URL
+$orders = selectOrdersByCustomer($customer_id); // Pass the customer_id to the function
 include 'view-orders-by-customer.php'; 
 include 'view-footer.php';
 ?>
