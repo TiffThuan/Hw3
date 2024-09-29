32561cca2490acf468893d45ab809c50e6ed5e8e
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 11:39:52 -0500

    Update and rename courses-by-instructor.php to orders-by-customer.php

---
+++ orders-by-customer.php
@@ -0,0 +1,12 @@
+
+<?php
+require_once ('util-db.php');
+require_once('model-orders-by-customer.php');
+
+$pageTitle = "Orders by Customer"; // Update title
+include 'view-header.php';
+
+$orders = selectOrdersByCustomer($_GET['id']); // Use customer_id instead of instructor id
+include 'view-orders-by-customer.php'; 
+include 'view-footer.php';
+?>
