f8654fd2f3ffa43832f4229a63d26c0253acfb8d
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-28 21:30:24 -0500

    Update view-sections-by-course.php

--- view-sections-by-course.php
+++ view-sections-by-course.php
@@ -1,31 +1,29 @@
-<h1>Sections By Course  </h1>
+<h1>Sections By Course</h1>
 <div class="table-responsive">
   <table class="table">
-   <thead>
-     <tr>
-       <th> ID </th>
-     <th>Total Amount</th>
-     <th>Status</th>
-     <th>Quantity</th>
-     <th>Price</th>
-     </tr>
-     
-   </thead> 
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Total Amount</th>
+        <th>Status</th>
+        <th>Quantity</th>
+        <th>Price</th>
+      </tr>
+    </thead>
     <tbody>
       <?php
-while ($section= $sections -> fetch_assoc()) {
-    ?>
-       <tr>
-         <td><?php echo $section['order_id'];?> </td>
-         <td><?php echo $section['total_amount'];?>  </td>
-         <td><?php echo $section['status'];?>  </td>
-         <td><?php echo $section['quantity'];?>  </td>
-         <td><?php echo $section['price'];?>  </td>
-        
-       </tr>
-       <?php
-}
-      ?>  
+      while ($section = $sections->fetch_assoc()) { 
+      ?>
+        <tr>
+          <td><?php echo $section['order_id']; ?></td>
+          <td><?php echo $section['total_amount']; ?></td>
+          <td><?php echo $section['status']; ?></td>
+          <td><?php echo $section['quantity']; ?></td>
+          <td><?php echo $section['price']; ?></td>
+        </tr>
+      <?php
+      } // Close while loop
+      ?>
     </tbody>
   </table>
 </div>
