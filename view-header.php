3d38cdd7055ec94f4abff038546d02c5168cba40
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 12:06:23 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -1,46 +1,29 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?= $pageTitle ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
-    <!-- Custom CSS -->
-    <style>
-      .navbar-nav .nav-link {
-        color: blue;
-      }
-      .navbar-nav .nav-link.active {
-        background-color: #003366;
-        color: white;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="container">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">HW3 - King Coffee Shop</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $pageTitle; ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">Coffee Shop</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link <?= $pageTitle == 'Welcome to Our King Coffee Shop' ? 'active' : '' ?>" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link <?= $pageTitle == 'Instructors' ? 'active' : '' ?>" href="instructors.php">Instructors</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link <?= $pageTitle == 'Course' ? 'active' : '' ?>" href="courses.php">Courses</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link <?= $pageTitle == 'Instructors With Courses' ? 'active' : '' ?>" href="instructors-with-courses.php">Instructor with courses</a>
-              </li>
-             
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="orders-by-customer.php">Orders</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="products.php">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="customers-with-orders.php">Customers</a>
+                </li>
             </ul>
-          </div>
         </div>
-      </nav>
+    </nav>
+    <div class="container mt-4">
