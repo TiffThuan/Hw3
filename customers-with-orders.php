cdffae9eb378236b7f3bc20b0308646ea170ee42
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 11:45:26 -0500

    Update and rename instructors-with-courses.php to customers-with-orders.php

---
+++ customers-with-orders.php
@@ -0,0 +1,13 @@
+
+
+<?php
+require_once('util-db.php');
+require_once('model-customers-with-orders.php'); // Corrected model import
+
+$pageTitle = "Customers with Orders";
+include 'view-header.php';
+
+$customersWithOrders = selectCustomersWithOrders();
+include 'view-customers-with-orders.php'; // Load view
+include 'view-footer.php';
+?>
