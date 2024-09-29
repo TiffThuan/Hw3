21a39440fde6705fa716ca7a44c7e48ebd32185a
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 15:03:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ include 'view-header.php';
 
 <div class="container">
     <h1>Welcome to the Coffee Shop Management System</h1>
-    <p>Use the navigation links above to manage customer orders, view product details, and track order details.</p>
+    <p>Use the navigation links above to manage customer orders, view order details, and track order details.</p>
     
     <ul>
         <li><a href="orders-by-customer.php">View Orders by Customer</a></li>
-        <li><a href="products.php">View Available Products</a></li>
+        <li><a href="orders.php">View Available Orders</a></li>
         <li><a href="customers-with-orders.php">View Customers with Orders</a></li>
     </ul>
 </div>
