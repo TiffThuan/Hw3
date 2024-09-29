7b0ac6a3f1acb1874baa18fbb6de655d9b57c86d
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 14:21:35 -0500

    Update model-details-by-order.php

--- model-details-by-order.php
+++ model-details-by-order.php
@@ -7,8 +7,8 @@ function selectDetailsByOrder($order_id) {
         $stmt = $conn->prepare("
             SELECT od.order_id, p.product_name, od.quantity, od.price 
             FROM order_details od
-            JOIN products p ON od.product_id = p.product_id
-            WHERE od.order_id = ?
+            JOIN products p ON od.product_id = p.productid
+            WHERE od.order_id = ?;
         ");
         $stmt->bind_param("i", $order_id); // Bind the order_id
         $stmt->execute();
