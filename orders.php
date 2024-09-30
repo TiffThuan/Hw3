76191f7a5e33e0b28b60e0e402e4690fc8f2210e
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 22:10:51 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -1,11 +1,18 @@
 
+
 <?php
-require_once ('util-db.php');
+require_once('util-db.php');
 require_once('model-orders.php');
 
-$pageTitle = "Orders"; // Update title
+$pageTitle = "Orders"; 
 include 'view-header.php';
-$orders = selectOrders(); // Fetch all orders
-include 'view-orders.php'; 
+
+$orders = selectOrders();
+if ($orders && $orders->num_rows > 0) {
+    include 'view-orders.php'; 
+} else {
+    echo "<p>No orders found.</p>";
+}
+
 include 'view-footer.php';
 ?>
