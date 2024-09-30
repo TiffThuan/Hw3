dc1a8b1c822004b5081d1b65e46a949dd91a067a
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 22:18:44 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,11 +1,8 @@
 <?php
-function get_db_connection(){
-    // Create connection
+function get_db_connection() {
     $conn = new mysqli('mis-4013db.mysql.database.azure.com', 'mis4013db', 'Hippolove123#', 'mycoffeeshop_database');
-    
-    // Check connection
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error); // Display error for debugging
+        throw new Exception("Connection failed: " . $conn->connect_error);
     }
     return $conn;
 }
