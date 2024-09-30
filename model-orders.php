f544e360eb3e445bd9476d35f004393dfd5f3206
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-29 22:15:36 -0500

    Update model-orders.php

--- model-orders.php
+++ model-orders.php
@@ -1,11 +1,12 @@
-
 <?php
 function selectOrders() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT o.order_id, c.firstname AS customer_name, o.order_date, o.total_amount, o.status 
+        $stmt = $conn->prepare("
+            SELECT o.order_id, c.firstname AS customer_name, o.order_date, o.total_amount, o.status 
             FROM orders o
-            JOIN customers c ON o.customer_id = c.customer_id");
+            JOIN customers c ON o.customer_id = c.customer_id
+        ");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -15,6 +16,4 @@ function selectOrders() {
         throw $e;
     }
 }
-
-
 ?>
