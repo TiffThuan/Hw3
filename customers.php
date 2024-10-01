3a668a4fe0b4c716ad67433728e39d7add4aae33
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-01 00:27:00 -0500

    Update customers.php

--- customers.php
+++ customers.php
@@ -1,23 +1,11 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-
-
-<?php
-require_once('util-db.php');
+require_once ('util-db.php');
 require_once('model-customers.php');
 
-$pageTitle = "Customers"; 
+$pageTitle = "Customers";
 include 'view-header.php';
 
 $customers = selectCustomers();
-if ($customers && $customers->num_rows > 0) {
-    include 'view-customers.php'; 
-} else {
-    echo "<p>No customers found.</p>";
-}
-
+include 'view-customers.php'; 
 include 'view-footer.php';
 ?>
