7cfecafbc77115d1e83e75c15062ce6dcf043397
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-30 22:06:26 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection() {
     // Create connection
-    $conn = new mysqli('mis-4013db.mysql.database.azure.com', 'mis4013db', 'Hippolove123#', 'mycoffeeshop_database');
+    $conn = new mysqli('mis-4013db.mysql.database.azure.com', 'tifft', 'Hippolove123#', 'mycoffeeshop_database');
     
     // Check connection
     if ($conn->connect_error) {
