47d04fa27cbc0e557fbbcbd488cdc3a927966216
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-01 14:39:10 -0500

    Update customers-with-orders.php

--- customers-with-orders.php
+++ customers-with-orders.php
@@ -1,12 +1,12 @@
 
 <?php
 require_once('util-db.php');
-require_once('model-customers-with-orders.php'); // Corrected model import
+require_once('model-customers-with-orders.php'); 
 
 $pageTitle = "Customers with Orders";
 include 'view-header.php';
+$customers = selectCustomers(); // Fetches all customers first before selectCustomersWithOrders()
 
-$customersWithOrders = selectCustomersWithOrders();
 include 'view-customers-with-orders.php';
 include 'view-footer.php';
 ?>
