8b035d48d1516a1aa107725a42f3e7d4a725c14a
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-01 00:28:05 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -1,19 +1,11 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
-
-<?php
-require_once('util-db.php');
+require_once ('util-db.php');
 require_once('model-orders.php');
 
 $pageTitle = "Orders"; 
 include 'view-header.php';
 
 $orders = selectOrders();
-
 include 'view-orders.php'; 
-
 include 'view-footer.php';
 ?>
