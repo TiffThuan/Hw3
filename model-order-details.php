5a357178106f9be590ed76c5c4b9106653145f09
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-01 00:32:42 -0500

    Update and rename model-details-by-order.php to model-order-details.php

---
+++ model-order-details.php
@@ -0,0 +1,19 @@
+<?php
+function selectOrderDetails($order_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare(" SELECT od.order_id, p.product_name, od.quantity, od.price 
+                                    FROM order_details od
+                                    JOIN products p ON od.product_id = p.productid
+                                    WHERE od.order_id = ?");
+        $stmt->bind_param("i", $order_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
