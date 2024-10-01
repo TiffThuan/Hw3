ff35971a61263cee9aeca22532343321a7c1a50e
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-01 00:27:45 -0500

    Update view-customers.php

--- view-customers.php
+++ view-customers.php
@@ -1,31 +1,29 @@
 <h1>Customers</h1>
 <div class="table-responsive">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Address</th>
-        <th>Orders</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      if ($customers->num_rows === 0) {
-          echo "<tr><td colspan='4'>No customers found.</td></tr>";
-      } else {
-          while ($customer = $customers->fetch_assoc()) {
-              ?>
-              <tr>
-                <td><?php echo htmlspecialchars($customer['customer_id']); ?></td>
-                <td><?php echo htmlspecialchars($customer['firstname'] . ' ' . $customer['lastname']); ?></td>
-                <td><?php echo htmlspecialchars($customer['address']); ?></td>
-                <td><a href="orders-by-customer.php?customer_id=<?php echo $customer['customer_id']; ?>">View Orders</a></td>
-              </tr>
-              <?php
-          }
-      }
-      ?>
-    </tbody>
-  </table>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($customer = $customers->fetch_assoc()) {
+            ?>
+                <tr>
+                    <td><?php echo htmlspecialchars($customer['customer_id']); ?></td>
+                    <td><?php echo htmlspecialchars($customer['firstname']) . ' ' . htmlspecialchars($customer['lastname']); ?></td>
+                    <td><?php echo htmlspecialchars($customer['email']); ?></td>
+                    <td><?php echo htmlspecialchars($customer['phone']); ?></td>
+                    <td><a href="orders-by-customer.php?customer_id=<?php echo $customer['customer_id']; ?>">View Orders</a></td>
+                </tr>
+            <?php
+            }
+            ?>  
+        </tbody>
+    </table>
 </div>
