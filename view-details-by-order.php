82395e00b6caf8f5a97b887e7a444822b0350860
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-09-30 20:20:43 -0500

    Update view-details-by-order.php

--- view-details-by-order.php
+++ view-details-by-order.php
@@ -12,18 +12,17 @@
     </thead>
     <tbody>
       <?php
-      // Loop through the details of the order
       while ($detail = $orderDetails->fetch_assoc()) { 
       ?>
         <tr>
           <td><?php echo $detail['order_id']; ?></td>
-          <td><?php echo number_format($detail['total_amount'], 2); ?></td>
+          <td><?php echo number_format($detail['total_amount'], 2); ?></td> <!-- Added comma for two decimal points -->
           <td><?php echo $detail['status']; ?></td>
           <td><?php echo $detail['quantity']; ?></td>
-          <td><?php echo number_format($detail['price'], 2); ?></td>
+          <td><?php echo number_format($detail['price'], 2); ?></td> <!-- Added comma for two decimal points -->
         </tr>
       <?php
-      } // Close the while loop
+      }
       ?>
     </tbody>
   </table>
