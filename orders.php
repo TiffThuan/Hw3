f3a6c06ca8512ba1906c1114f7bd1380c3a4a324
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-01 20:52:32 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -2,13 +2,15 @@
 require_once('util-db.php');
 require_once('model-orders.php');
 
-ini_set('display_errors', 1); 
-error_reporting(E_ALL);
-
 $pageTitle = "Orders"; 
 include 'view-header.php';
 
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+// Fetch all orders without filtering by customer
 $orders = selectOrders();
+
 if ($orders->num_rows === 0) {
     echo "No orders found!";
 } else {
