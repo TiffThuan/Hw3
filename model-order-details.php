91be513f068a0d090e556f5f8e73eb6c63816921
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-01 22:10:32 -0500

    Update model-order-details.php

--- model-order-details.php
+++ model-order-details.php
@@ -1,4 +1,24 @@
 <?php
+function selectOrders() {
+    $conn = null;
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT o.order_id, o.order_date, c.firstname, c.lastname, o.total_amount 
+                                FROM orders o 
+                                JOIN customers c ON o.customer_id = c.customer_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        
+        return $result;
+    } catch (Exception $e) {
+        throw $e;
+    } finally {
+        if ($conn) {
+            $conn->close();
+        }
+    }
+}
 function selectOrderDetails($order_id) {
     try {
         $conn = get_db_connection();
