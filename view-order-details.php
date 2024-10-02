9621d6a5ab2a95e619863f80b639dfe64dbf62cf
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-01 22:43:59 -0500

    Update view-order-details.php

--- view-order-details.php
+++ view-order-details.php
@@ -1,26 +1,32 @@
-<h1>Order Details</h1>
+<h1>Order Details for Order ID: <?php echo htmlspecialchars($order_id); ?></h1>
+
 <div class="table-responsive">
     <table class="table">
         <thead>
             <tr>
-                <th>Product Name</th>
+                <th>Product</th>
                 <th>Quantity</th>
                 <th>Price</th>
+                <th>Total</th>
             </tr>
         </thead>
         <tbody>
             <?php
-            while ($detail = $orderDetails->fetch_assoc()) {
-                ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($detail['product_name']); ?></td>
-                    <td><?php echo htmlspecialchars($detail['quantity']); ?></td>
-                    <td><?php echo htmlspecialchars($detail['price']); ?></td>
-                </tr>
-                <?php
-            }
+            if ($orderDetails && $orderDetails->num_rows > 0) {
+                while ($detail = $orderDetails->fetch_assoc()) {
             ?>
+                    <tr>
+                        <td><?php echo htmlspecialchars($detail['product_name']); ?></td>
+                        <td><?php echo htmlspecialchars($detail['quantity']); ?></td>
+                        <td><?php echo htmlspecialchars($detail['price']); ?></td>
+                        <td><?php echo htmlspecialchars($detail['quantity'] * $detail['price']); ?></td>
+                    </tr>
+            <?php
+                }
+            } else {
+                echo "<tr><td colspan='4'>No order details found.</td></tr>";
+            }
+            ?>  
         </tbody>
     </table>
 </div>
-
