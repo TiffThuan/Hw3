d788fd57b02e096e140093a4330717ad4b6befee
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-02 20:26:35 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -10,7 +10,17 @@ require_once('model-orders.php');
 $pageTitle = "Orders";
 include 'view-header.php';
 
-$orders = selectOrders(); 
+// Check if a product_id is passed via the query string
+$product_id = isset($_GET['product_id']) ? intval($_GET['product_id']) : 0;
+
+// Fetch orders, optionally filtered by product_id
+if ($product_id > 0) {
+    // If product_id is provided, get only the orders for that product
+    $orders = selectOrdersByProduct($product_id);
+} else {
+    // Otherwise, get all orders
+    $orders = selectOrders();
+}
 
 include 'view-orders.php';
 include 'view-footer.php';
