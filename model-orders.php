0494d28f09efacf4c3d8f59cdb4fa3348325d01c
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-02 20:30:44 -0500

    Update model-orders.php

--- model-orders.php
+++ model-orders.php
@@ -41,5 +41,18 @@ function selectOrderDetails($order_id) {
         throw $e;
     }
 }
+function selectProducts() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT productid, product_name, product_description, price FROM products");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 ?>
