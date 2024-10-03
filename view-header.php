32166b83228992a6e1329443838f84e3eeae0d01
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-03 14:44:55 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,20 +8,23 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">King Coffee Shop</a>
+        <a class="navbar-brand" href="index.php">King Coffee Shop</a> <!-- Home page link -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="orders.php">Orders</a>
+                    <a class="nav-link" href="index.php">Home</a> <!-- Home tab -->
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="products.php">Products</a>
+                    <a class="nav-link" href="orders.php">Orders</a> <!-- Link to Orders -->
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="customers.php">Customers</a>
+                    <a class="nav-link" href="products.php">Products</a> <!-- Link to Products -->
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="customers.php">Customers</a> <!-- Link to Customers -->
                 </li>
             </ul>
         </div>
