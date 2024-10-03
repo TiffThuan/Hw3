b455a73e7971712d781f93b1bff57f4cf2a58542
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-02 19:19:41 -0500

    Update view-products.php

--- view-products.php
+++ view-products.php
@@ -19,7 +19,8 @@
                     <td><?php echo htmlspecialchars($product['product_name']); ?></td>
                     <td><?php echo htmlspecialchars($product['product_description']); ?></td>
                     <td><?php echo htmlspecialchars($product['price']); ?></td>
-                    <td><a href="order.php?order_id=<?php echo $order['order_id']; ?>">View Orders</a></td>
+                    <!-- Correct the hyperlink -->
+                    <td><a href="orders.php?product_id=<?php echo htmlspecialchars($product['productid']); ?>">View Orders</a></td>
                 </tr>
             <?php
             }
