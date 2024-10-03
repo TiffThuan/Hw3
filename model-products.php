e9d7877e0ccc0f4aa4c873fd83d89a2976f54cc1
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-02 19:02:29 -0500

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -15,9 +15,10 @@ function selectProducts() {
 function selectOrders() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT o.order_id, o.order_date, c.firstname, c.lastname, o.total_amount 
-                                FROM orders o 
-                                JOIN customers c ON o.customer_id = c.customer_id");
+        $stmt = $conn->prepare("SELECT o.order_id, c.firstname AS customer_name, o.order_date, o.total_amount, o.status 
+                                FROM orders o
+                                JOIN customers c ON o.customer_id = c.customer_id
+                                WHERE o.customer_id =?");
         $stmt->execute();
         $result = $stmt->get_result();
         $stmt->close();
