3cc02db3d35064cae00098468d53423284f2acca
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-04 17:29:15 -0500

    Update model-order-details.php

--- model-order-details.php
+++ model-order-details.php
@@ -1,24 +1,5 @@
 <?php
-function selectOrders() {
-    $conn = null;
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT o.order_id, o.order_date, c.firstname, c.lastname, o.total_amount 
-                                FROM orders o 
-                                JOIN customers c ON o.customer_id = c.customer_id");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $stmt->close();
-        
-        return $result;
-    } catch (Exception $e) {
-        throw $e;
-    } finally {
-        if ($conn) {
-            $conn->close();
-        }
-    }
-}
+
 // Function to fetch order details by order ID
 function selectOrderDetails($order_id) {
     $conn = null;
@@ -35,6 +16,27 @@ function selectOrderDetails($order_id) {
         $result = $stmt->get_result();
         $stmt->close();
 
+        return $result;
+    } catch (Exception $e) {
+        throw $e;
+    } finally {
+        if ($conn) {
+            $conn->close();
+        }
+    }
+}
+
+function selectOrders() {
+    $conn = null;
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT o.order_id, o.order_date, c.firstname, c.lastname, o.total_amount 
+                                FROM orders o 
+                                JOIN customers c ON o.customer_id = c.customer_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $stmt->close();
+        
         return $result;
     } catch (Exception $e) {
         throw $e;
