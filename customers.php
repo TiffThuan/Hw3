b46355a07b3ec03d9f8d829643dcb73b67b5327b
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-05 09:54:30 -0500

    Update customers.php

--- customers.php
+++ customers.php
@@ -5,10 +5,10 @@ require_once('model-customers.php');
 $pageTitle = "Customers"; 
 include 'view-header.php';
 
-if (isset ($_POST['actionType])){
-switch($_POST['actionType]){
+if (isset ($_POST['actionType'])){
+switch($_POST['actionType']){
     case "Add":
-      insertCustomers($_POST[cFName'],$_POST[cLName'],$_POST[cEmail'],$_POST[cPhone']);
+      insertCustomers($_POST['cFName'],$_POST['cLName'],$_POST['cEmail'],$_POST['cPhone']);
     break;
     }
 }
