231952118e5aa49cd8a4789afb2a1b113652eccd
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-05 12:32:38 -0500

    Update customers.php

--- customers.php
+++ customers.php
@@ -5,33 +5,46 @@ require_once('model-customers.php');
 $pageTitle = "Customers"; 
 include 'view-header.php';
 
-if (isset ($_POST['actionType'])){
-switch($_POST['actionType']){
-    case "Add":
-      if (insertCustomers($_POST['cFName'],$_POST['cLName'],$_POST['cEmail'],$_POST['cPhone'])) {
-         echo '<div class = "alert alert-success" role= "alert"> Customer added ....</div>'; 
-      } else {
-          echo '<div class = "alert alert-danger" role= "alert"> Error added ....</div>';  
-      }
-    break;
+echo "<pre>";
+print_r($_POST); // Debugging POST data
+echo "</pre>";
 
-      case "Edit":
-      if (updateCustomers($_POST['cFName'],$_POST['cLName'],$_POST['cEmail'],$_POST['cPhone'],$_POST['cid'])) {
-         echo '<div class = "alert alert-success" role= "alert"> Customer edited ....</div>'; 
-      } else {
-          echo '<div class = "alert alert-danger" role= "alert"> Error edited ....</div>';  
-      }
-    break;
-    
-    case "Delete":
-      if (deleteCustomers($_POST['cid'])) {
-         echo '<div class = "alert alert-success" role= "alert"> Customer deleted ....</div>'; 
-      } else {
-          echo '<div class = "alert alert-danger" role= "alert"> Error deleted ....</div>';  
-      }
-    break;
+if (isset ($_POST['actionType'])) {
+    switch($_POST['actionType']) {
+        case "Add":
+            if (insertCustomers($_POST['cFName'], $_POST['cLName'], $_POST['cEmail'], $_POST['cPhone'])) {
+                echo '<div class = "alert alert-success" role= "alert"> Customer added ....</div>'; 
+            } else {
+                echo '<div class = "alert alert-danger" role= "alert"> Error added ....</div>';  
+            }
+            break;
+
+        case "Edit":
+            if (isset($_POST['cid'], $_POST['cFName'], $_POST['cLName'], $_POST['cEmail'], $_POST['cPhone'])) {
+                if (updateCustomers($_POST['cFName'], $_POST['cLName'], $_POST['cEmail'], $_POST['cPhone'], $_POST['cid'])) {
+                    echo '<div class = "alert alert-success" role= "alert"> Customer edited ....</div>'; 
+                } else {
+                    echo '<div class = "alert alert-danger" role= "alert"> Error edited ....</div>';  
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert"> Missing data for editing.</div>';
+            }
+            break;
+
+        case "Delete":
+            if (isset($_POST['cid'])) {
+                if (deleteCustomers($_POST['cid'])) {
+                    echo '<div class = "alert alert-success" role= "alert"> Customer deleted ....</div>'; 
+                } else {
+                    echo '<div class = "alert alert-danger" role= "alert"> Error deleted ....</div>';  
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert"> Invalid or missing customer ID.</div>';
+            }
+            break;
     }
 }
+
 $customers = selectCustomers();
 include 'view-customers.php'; 
 include 'view-footer.php';
