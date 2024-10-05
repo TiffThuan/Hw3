11719923cecc11cbf286a094dfd9863b0ba36cab
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-04 22:31:03 -0500

    Update model-customers.php

--- model-customers.php
+++ model-customers.php
@@ -12,5 +12,60 @@ function selectCustomers() {
         throw $e;
     }
 }
+
+function insertCustomers($cFName, $cLName, $cEmail, $cPhone) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `mycoffeeshop_database`.`customers`
+                                (`firstname`,
+                                `lastname`,
+                                `email`,
+                                `phone`)
+                                VALUES
+                                (?,?,?,?);");
+        $stmt->bind_param("ssss", $cFName, $cLName, $cEmail, $cPhone);   //ss: number of string variables
+        $success = $stmt->execute();
+    
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCustomers($cFName, $cLName, $cEmail, $cPhone,$cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `mycoffeeshop_database`.`customers` SET
+                                (`firstname` = ?,
+                                `lastname`= ?,
+                                `email`=?,
+                                `phone`=?) WHERE customer_id = ?");
+        $stmt->bind_param("ssssi", $cFName, $cLName, $cEmail, $cPhone,$cid);   
+        $success = $stmt->execute();
+    
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCustomers($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `mycoffeeshop_database`.`customers` WHERE customer_id = ?");
+        $stmt->bind_param("i", $cid);   // i: one interger value
+        $success = $stmt->execute();
+    
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
 
