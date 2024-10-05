72a0c1233bcceba7c252e41d080db7d0c8958409
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-05 17:34:12 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -9,6 +9,42 @@ require_once('model-orders.php');
 $pageTitle = "Orders";
 include 'view-header.php';
 
+if (isset ($_POST['actionType'])) {
+    switch($_POST['actionType']) {
+        case "Add":
+            if (insertOrder($_POST['customer_id'], $_POST['total_amount'])) {
+                echo '<div class = "alert alert-success" role= "alert"> Order added ....</div>'; 
+            } else {
+                echo '<div class = "alert alert-danger" role= "alert"> Error adding order ....</div>';  
+            }
+            break;
+
+        case "Edit":
+            if (isset($_POST['order_id'], $_POST['customer_id'], $_POST['total_amount'])) {
+                if (updateOrder($_POST['order_id'], $_POST['customer_id'], $_POST['total_amount'])) {
+                    echo '<div class = "alert alert-success" role= "alert"> Order edited ....</div>'; 
+                } else {
+                    echo '<div class = "alert alert-danger" role= "alert"> Error editing order ....</div>';  
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert"> Missing data for editing.</div>';
+            }
+            break;
+
+        case "Delete":
+            if (isset($_POST['order_id'])) {
+                if (deleteOrder($_POST['order_id'])) {
+                    echo '<div class = "alert alert-success" role= "alert"> Order deleted ....</div>'; 
+                } else {
+                    echo '<div class = "alert alert-danger" role= "alert"> Error deleting order ....</div>';  
+                }
+            } else {
+                echo '<div class="alert alert-danger" role="alert"> Invalid or missing order ID.</div>';
+            }
+            break;
+    }
+}
+
 $orders = selectOrders(); 
 
 
