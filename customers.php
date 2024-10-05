7ff819cd91b5ba692c77291c97e305d914cbf3af
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-05 11:37:04 -0500

    Update customers.php

--- customers.php
+++ customers.php
@@ -14,6 +14,14 @@ switch($_POST['actionType']){
           echo '<div class = "alert alert-danger" role= "alert"> Error added ....</div>';  
       }
     break;
+
+        case "Delete":
+      if (deleteCustomers($_POST['cid'])) {
+         echo '<div class = "alert alert-success" role= "alert"> Customer deleted ....</div>'; 
+      } else {
+          echo '<div class = "alert alert-danger" role= "alert"> Error deleted ....</div>';  
+      }
+    break;
     }
 }
 $customers = selectCustomers();
