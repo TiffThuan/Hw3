d2f893d5a085fea92c8b105e3de09017080a7945
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-05 11:17:50 -0500

    Update customers.php

--- customers.php
+++ customers.php
@@ -8,7 +8,11 @@ include 'view-header.php';
 if (isset ($_POST['actionType'])){
 switch($_POST['actionType']){
     case "Add":
-      insertCustomers($_POST['cFName'],$_POST['cLName'],$_POST['cEmail'],$_POST['cPhone']);
+      if (insertCustomers($_POST['cFName'],$_POST['cLName'],$_POST['cEmail'],$_POST['cPhone'])) {
+         echo '<div class = "alert alert-success" role= "alert"> Customer added ....</div>'; 
+      } else {
+          echo '<div class = "alert alert-danger" role= "alert"> Error added ....</div>';  
+      }
     break;
     }
 }
