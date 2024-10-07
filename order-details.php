c7191efaef7f2feae2246886dabe5fb0dd06f7fa
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-07 17:41:01 -0500

    Update order-details.php

--- order-details.php
+++ order-details.php
@@ -1,6 +1,6 @@
 <?php
 require_once('util-db.php');
-require_once('model-orders.php');
+require_once('model-orders.php'); // Use model-orders.php for order retrieval functions
 
 $pageTitle = "Order Details";
 include 'view-header.php';
@@ -9,14 +9,14 @@ include 'view-header.php';
 ini_set('display_errors', 1); 
 error_reporting(E_ALL);
 
-// Get the order_id from the POST data
+// Get the order_id from POST request
 $order_id = isset($_POST['order_id']) ? intval($_POST['order_id']) : null;
 
 if ($order_id) {
     $orderDetails = selectOrderDetails($order_id);
 
     if ($orderDetails && $orderDetails->num_rows > 0) {
-        include 'view-order-details.php';
+        include 'view-order-details.php'; // Template to display the details of the order
     } else {
         echo "<p>No details found for this order.</p>";
     }
