a904857ec9c2ed22dfe72e43f09f0d57b3430028
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-11 00:10:42 -0500

    Update products.php

--- products.php
+++ products.php
@@ -9,6 +9,23 @@ require_once('model-reviews.php'); // Include reviews model
 
 $pageTitle = "Products"; 
 include 'view-header.php';
+// Handle form submissions for adding, editing, and deleting products
+if (isset($_POST['actionType'])) {
+    // Check action type (add, edit, delete)
+    if (isset($_POST['actionType'])) {
+        switch ($_POST['actionType']) {
+            case 'addProduct':
+                // Call function to add product
+                break;
+            case 'editProduct':
+                // Call function to update product
+                break;
+            case 'deleteProduct':
+                // Call function to delete product
+                break;
+        }
+    }
+}
 
 $products = selectProducts();
 include 'view-products.php'; 
