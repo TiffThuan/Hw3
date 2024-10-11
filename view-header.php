d45a5fc7d9d04a9845cd573443f71710a94638e8
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-10 22:53:21 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,23 +8,26 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">King Coffee Shop</a> <!-- Home page link -->
+        <a class="navbar-brand" href="index.php">King Coffee Shop</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">Home</a> <!-- Home tab -->
+                    <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="orders.php">Orders</a> <!-- Link to Orders -->
+                    <a class="nav-link" href="orders.php">Orders</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="customers.php">Customers</a> <!-- Link to Customers -->
+                    <a class="nav-link" href="customers.php">Customers</a>
                 </li>
-                 <li class="nav-item">
-                    <a class="nav-link" href="products.php">Products</a> <!-- Link to Products -->
+                <li class="nav-item">
+                    <a class="nav-link" href="products.php">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reviews.php">Reviews</a> <!-- Link to Reviews -->
                 </li>
             </ul>
         </div>
