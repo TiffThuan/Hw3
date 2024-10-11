3968debcfd9b9917cd93488855fbb9097a6dfca8
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-10 23:53:34 -0500

    Update view-reviews-editform.php

--- view-reviews-editform.php
+++ view-reviews-editform.php
@@ -6,6 +6,7 @@
                 <h5 class="modal-title" id="editReviewModalLabel">Edit Review</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            
             <div class="modal-body">
                 <form method="POST" action="reviews.php">
                     <input type="hidden" name="actionType" value="editReview">
