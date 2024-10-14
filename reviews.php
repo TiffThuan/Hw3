cf322d794eff2c9fcd7bdf66dd5759f94e04a160
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-13 20:08:26 -0500

    Update reviews.php

--- reviews.php
+++ reviews.php
@@ -45,31 +45,8 @@ if (isset($_POST['actionType'])) {
     }
 }
 
-// Fetch reviews
-$product_id = isset($_GET['product_id']) ? intval($_GET['product_id']) : 0;
-$reviews = fetchReviewsByProduct($product_id);
-?>
-<h1>Reviews for Product ID: <?php echo $product_id; ?></h1>
-<div class="reviews">
-    <ul>
-        <?php if ($reviews && $reviews->num_rows > 0) { ?>
-            <?php while ($review = $reviews->fetch_assoc()) { ?>
-                <li>
-                    <strong>Rating:</strong> <?php echo $review['rating']; ?> 
-                    <br>
-                    <strong>Review:</strong> <?php echo htmlspecialchars($review['review_text']); ?>
-                    <br>
-                    <button data-bs-toggle="modal" data-bs-target="#editReviewModal<?php echo $review['review_id']; ?>">Edit</button>
-                    <button onclick="confirmDelete(<?php echo $review['review_id']; ?>)">Delete</button>
-                </li>
-                <?php include 'view-reviews-editform.php'; ?>
-            <?php } ?>
-        <?php } else { ?>
-            <li>No reviews yet.</li>
-        <?php } ?>
-    </ul>
-</div>
-<?php include 'view-reviews-newform.php'; ?>
+$reviews=selectReviews(product_id);
+<?php include 'view-reviews.php'; ?>
 
 
 <?php include 'view-footer.php'; ?>
