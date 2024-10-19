7343f5ac86fb2c433b7222699dd6a816e82472d6
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-19 12:48:36 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -11,26 +11,26 @@ require_once('model-orders.php');
 $pageTitle = "Orders";
 include 'view-header.php';
 
-if (isset ($_POST['actionType'])) {
-    switch($_POST['actionType']) {
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
         case "Add":
-             if (isset( $_POST['order_date'], $_POST['cFname'], $_POST['cLname'], $_POST['total_amount'])) {
-                if (insertOrder($_POST['order_date'], $_POST['cFname'], $_POST['cLname'], $_POST['total_amount'])) {
-                    echo '<div class="alert alert-success" role="alert">Order added successfully.</div>'; 
+            if (isset($_POST['order_date'], $_POST['cFName'], $_POST['cLName'], $_POST['email'], $_POST['total_amount'])) {
+                if (insertOrder($_POST['order_date'], $_POST['cFName'], $_POST['cLName'], $_POST['email'], $_POST['total_amount'])) {
+                    echo '<div class="alert alert-success" role="alert">Order added...</div>';
                 } else {
-                    echo '<div class="alert alert-danger" role="alert">Error adding order.</div>';  
+                    echo '<div class="alert alert-danger" role="alert">Error adding order...</div>';
                 }
-             } else {
+            } else {
                 echo '<div class="alert alert-danger" role="alert">Missing data for adding.</div>';
-             }
-             break;
+            }
+            break;
 
         case "Edit":
-            if (isset($_POST['order_id'], $_POST['order_date'], $_POST['cFname'], $_POST['cLname'], $_POST['total_amount'])) {
-                if (updateOrder($_POST['order_date'], $_POST['cFname'], $_POST['cLname'], $_POST['total_amount'], $_POST['order_id'])) {
-                    echo '<div class="alert alert-success" role="alert">Order edited successfully.</div>'; 
+            if (isset($_POST['order_id'], $_POST['order_date'], $_POST['cFName'], $_POST['cLName'], $_POST['email'], $_POST['total_amount'])) {
+                if (updateOrder($_POST['order_id'], $_POST['order_date'], $_POST['cFName'], $_POST['cLName'], $_POST['email'], $_POST['total_amount'])) {
+                    echo '<div class="alert alert-success" role="alert">Order edited...</div>';
                 } else {
-                    echo '<div class="alert alert-danger" role="alert">Error editing order.</div>';  
+                    echo '<div class="alert alert-danger" role="alert">Error editing order...</div>';
                 }
             } else {
                 echo '<div class="alert alert-danger" role="alert">Missing data for editing.</div>';
@@ -40,9 +40,9 @@ if (isset ($_POST['actionType'])) {
         case "Delete":
             if (isset($_POST['order_id'])) {
                 if (deleteOrder($_POST['order_id'])) {
-                    echo '<div class="alert alert-success" role="alert">Order deleted successfully.</div>'; 
+                    echo '<div class="alert alert-success" role="alert">Order deleted...</div>';
                 } else {
-                    echo '<div class="alert alert-danger" role="alert">Error deleting order.</div>';  
+                    echo '<div class="alert alert-danger" role="alert">Error deleting order...</div>';
                 }
             } else {
                 echo '<div class="alert alert-danger" role="alert">Invalid or missing order ID.</div>';
@@ -50,6 +50,7 @@ if (isset ($_POST['actionType'])) {
             break;
     }
 }
+
 
 $orders = selectOrders(); 
 
