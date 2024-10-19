8bc42139fba4b7ce68d3683bf3f0dce9844c9c58
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-19 11:32:11 -0500

    Update orders.php

--- orders.php
+++ orders.php
@@ -3,6 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
 
+var_dump($_POST);  // Add this to inspect form data
+
 require_once('util-db.php');
 require_once('model-orders.php');
 
