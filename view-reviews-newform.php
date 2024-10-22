cbb8a2820edb79fafa4b9aec585c27133d8445cb
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-10-21 22:36:54 -0500

    Update view-reviews-newform.php

--- view-reviews-newform.php
+++ view-reviews-newform.php
@@ -2,7 +2,7 @@
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newReviewModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
         <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.32-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.63.283.95l-3.524 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-    </svg> Add Review
+    </svg> Add
 </button>
 
 <!-- Modal -->
