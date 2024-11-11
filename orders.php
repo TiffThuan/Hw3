7c43b16075c91fb762e5fa74dd55c9eb15fe16d3
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-11-10 19:21:14 -0600

    Update orders.php

--- orders.php
+++ orders.php
@@ -3,12 +3,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
 
+$pageTitle = "Orders";
 include 'view-header.php';
 require_once('util-db.php');
 require_once('model-orders.php');
 
 // Fetch data from the database
-$pageTitle = "Orders";
+
 $orders = selectOrders(); // Get the orders
 $menuPercentages = calculateMenuPercentages(); // Get menu percentages
 
