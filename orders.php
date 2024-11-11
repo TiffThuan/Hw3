ce94992ff9a048a4b97783096efdcd468cfb1b68
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-11-10 19:20:42 -0600

    Update orders.php

--- orders.php
+++ orders.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1); 
 error_reporting(E_ALL);
 
+include 'view-header.php';
 require_once('util-db.php');
 require_once('model-orders.php');
 
