f3f893eda13ace0c6381e6db9264bfc005b66cfe
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-11-14 19:57:11 -0600

    Update customers.php

--- customers.php
+++ customers.php
@@ -2,6 +2,10 @@
 require_once('util-db.php');
 require_once('model-customers.php');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $pageTitle = "Customers";
 include 'view-header.php';
 
