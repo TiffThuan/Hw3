9f26691670ecf8711b68aee317710f66a3da2873
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-11-21 23:11:47 -0600

    Update view-reviews.php

--- view-reviews.php
+++ view-reviews.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Find Us & Reviews - King Coffee Shop</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
         .section-header {
             font-size: 1.5rem;
