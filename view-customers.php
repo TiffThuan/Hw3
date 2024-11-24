c06687c5e53b723d67c6ef31f90f066688cb742d
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-11-24 16:37:11 -0600

    Update view-customers.php

--- view-customers.php
+++ view-customers.php
@@ -133,7 +133,7 @@
                 <div class="text-center alert alert-warning">No customer data available.</div>
             <?php endif; ?>
         </div>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/echarts/dist/echarts.min.js"></script>
     <script>
