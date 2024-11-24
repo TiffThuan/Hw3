b38c3d88352934e8981c83c9b9f1d56aedfd1a8a
TiffThuan <117796555+TiffThuan@users.noreply.github.com>
2024-11-24 14:56:01 -0600

    Update view-customers-newform.php

--- view-customers-newform.php
+++ view-customers-newform.php
@@ -1,6 +1,6 @@
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#newCustomerModal">
-    Add Customer
+    Register New Customer
 </button>
 
 <!-- Modal -->
@@ -8,7 +8,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="newCustomerModalLabel">Add New Customer</h5>
+                <h5 class="modal-title" id="newCustomerModalLabel">Register New Customer</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
